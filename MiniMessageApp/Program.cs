61207b65788a8d03301aba876fd677d08ac4474b
GET Academy <terje@kolderup.net>
2026-03-12 11:25:17 +0100

    x

--- MiniMessageApp/Program.cs
+++ MiniMessageApp/Program.cs
@@ -1,36 +1,60 @@
-// https://get-mini-messages
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    // Slår på autentisering i API-et.
+    // API-et forventer nå at klienten sender et JWT-token i headeren:
+    // Authorization: Bearer <token>
+
+    .AddJwtBearer(options =>
+        // Konfigurerer hvordan JWT-tokenet skal valideres.
+        // Når en request kommer inn skjer dette:
+        // 1. API-et leser Authorization-headeren
+        // 2. JWT-tokenet valideres
+        // 3. Claims fra tokenet gjøres tilgjengelige i requesten
+    {
+        options.Authority = "https://getacademy.eu.auth0.com";
+        // URL til Auth0-tenantet ditt.
+        // API-et bruker denne til å hente informasjon om hvem som utsteder tokenet
+        // og hvordan signaturen skal verifiseres.
+
+        options.Audience = "https://get-mini-messages";
+        // Audience sier hvilket API tokenet er ment for.
+        // Hvis tokenet ikke er laget for dette API-et blir requesten avvist.
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("read:messages", policy =>
+        // Lager en autorisasjonspolicy med navnet "read:messages".
+        // Endpoints kan kreve denne policyen for å få tilgang.
+
+        policy.RequireClaim("permissions", "read:messages"));
+    // Krever at JWT-tokenet inneholder denne claimen:
+    // "permissions": ["read:messages"]
+    // Hvis tokenet mangler denne tillatelsen returnerer API-et 403 Forbidden.
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles();
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+var messages = new List<string>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Hello",
+    "Secure world"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapGet("/public", () => "Public endpoint");
+
+app.MapGet("/messages", () => {
+        //var user = context.User.Identity?.Name ?? "Unknown user";
+        return messages;
+    })
+    .RequireAuthorization("read:messages");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
